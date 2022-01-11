3baf5c381d18c9e8f9729da3232acafc9cb23eb0
kim <yuyukgh@naver.com>
2022-01-11 22:02:47 +0900

    Chore : 추가 프리티어 및 환경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  parser: "@babel/eslint-parser",
+  parser: '@babel/eslint-parser',
   env: {
     browser: true,
     es2021: true,
@@ -7,25 +7,26 @@ module.exports = {
     jest: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     babelOptions: {
-      presets: ["@babel/preset-react"],
+      presets: ['@babel/preset-react'],
     },
     requireConfigFile: false,
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 13,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "import", "react-hooks"],
+  plugins: ['react', 'import', 'react-hooks'],
   rules: {
-    "react/react-in-jsx-scope": "off", // 리엑트 기본 import문을 생략해도 가능
-    "no-unused-vars": "error", // 사용하지않는 변수 error처리
-    "no-console": "off", // 콘솔로그를 사용하수 있음
+    'react/react-in-jsx-scope': 'off', // 리엑트 기본 import문을 생략해도 가능
+    'no-unused-vars': 'error', // 사용하지않는 변수 error처리
+    'no-console': 'off', // 콘솔로그를 사용하수 있음
   },
 };
