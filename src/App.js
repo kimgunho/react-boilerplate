7788599b6ea4e54f4e14560e6c7cb4d2980acfc1
kim <yuyukgh@naver.com>
2022-01-12 10:58:49 +0900

    Feat: 추가 스타일링 라이브러리(방식 : sass, classnames)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import classNames from 'classnames/bind';
+
+import styles from './App.module.scss';
+
+const cx = classNames.bind(styles);
+
 function App() {
-  return <div className="App"></div>;
+  return <div className={cx('app')}></div>;
 }
 
 export default App;
