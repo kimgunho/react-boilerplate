cb613768475eb04806bb0a49523c1acc3b4149a2
kim <yuyukgh@naver.com>
2022-01-12 11:52:59 +0900

    Add : 추가 초기 폴더트리 환경구성 및 컴포넌트 구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import styles from './App.module.scss';
+import { links } from './assets/data/links';
+
+import Home from './pages/home';
+import NotFound from './components/shared/NotFount';
+import Header from './components/shared/Header';
+import Footer from './components/shared/Footer';
 
 const cx = classNames.bind(styles);
 
 function App() {
-  return <div className={cx('app')}></div>;
+  return (
+    <div className={cx('app')}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={links.home} element={<Home />} />
+          <Route path={links.notFount} element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
